4dae5629542e767fd688e010cafdf300ef7f3d81
PiWalker <PiWalker@PiWalker-PC>
2015-09-05 14:54:47 -0600

    SetupThings

--- src/com/piwalker/FBLAGame/Main.java
+++ src/com/piwalker/FBLAGame/Main.java
@@ -1,5 +1,38 @@
 package com.piwalker.FBLAGame;
 
-public class Main {
 
+import com.piwalker.FBLAGame.References.References;
+
+import io.brace.lightsoutgaming.engine.LightsOut;
+
+public class Main extends LightsOut{
+	public static void main(String[] args){
+		new Main().init();
+	}
+	
+	public float aspectRatio = 9f/16f;
+	public int WIDTH = 900;
+	public int HEIGHT = (int) (WIDTH * aspectRatio);
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		System.out.println(aspectRatio);
+		this.createDisplay(References.TITLE_BAR, WIDTH, HEIGHT);
+		this.start();
+	}
+
+	@Override
+	protected void render() {
+		// TODO Auto-generated method stub
+		screen.clear();
+		
+		show();
+	}
+
+	@Override
+	protected void update() {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ src/com/piwalker/FBLAGame/References/References.java
@@ -0,0 +1,5 @@
+package com.piwalker.FBLAGame.References;
+
+public class References {
+	public static String TITLE_BAR = "FBLA Game";
+}
